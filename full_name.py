0eb5d64eb14c0c2ac51f0678db9bc1eca70de778
Franklin J Lee <40707294+Retrockit@users.noreply.github.com>
2022-12-05 07:42:33 -0500

    Initial commit

---
+++ full_name.py
@@ -0,0 +1,7 @@
+first_name = "monkey"
+middle_name = "d."
+last_name = "luffy"
+
+full_name = f"{first_name} {middle_name} {last_name}"
+
+print(full_name)
---
+++ name.py
@@ -0,0 +1,6 @@
+name = "frank j lee"
+print(name.title())
+
+print(name.upper())
+
+print(name.lower())
