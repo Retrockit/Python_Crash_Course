4031a6269ec6524e54475a56972d3b778dd58019
Franklin J Lee <40707294+Retrockit@users.noreply.github.com>
2022-12-06 18:49:17 -0500

    added files

--- full_name.py
+++ full_name.py
@@ -2,6 +2,6 @@ first_name = "monkey"
 middle_name = "d."
 last_name = "luffy"
 
-full_name = f"{first_name} {middle_name} {last_name}"
+full_name = f"{first_name.lower()} {middle_name.lower()} {last_name.lower()}"
 
 print(full_name)
---
+++ string.py
@@ -0,0 +1,11 @@
+First = "Frank"
+
+Middle = "J."
+
+Last = "Lee"
+
+message = "Hi my name is"
+
+message2 = "and I want to be an SRE"
+
+print(f"{message} {First.lower()} {Middle.lower()} {Last.lower()} \n{message2}")
